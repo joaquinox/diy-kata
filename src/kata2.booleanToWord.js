a66b5d66f7cb8d185562c58fc69026145c9e36cf
Sam Jones <samueldjones24@gmail.com>
2019-02-15 17:27:33 +0000

    Completed booleantoWord test

--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,5 +1,11 @@
 const booleanToWord = (boolean) => {
+   if (boolean.toString() === 'true') {
+       return 'Yes'
+   };
 
-}
+   if (boolean.toString() === 'false') {
+       return 'No'
+   };
+};
 
 module.exports = booleanToWord;
