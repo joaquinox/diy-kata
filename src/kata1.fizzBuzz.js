cf2c52c5dd114a5b5d1dd01f1ce70813b5ad58c2
Sam Jones <samueldjones24@gmail.com>
2019-02-15 16:26:21 +0000

    Completed fizzBuzz test

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,5 +1,18 @@
 const fizzBuzz = (number) => {
+    const isDivisibleBy3 = (number % 3) === 0;
+    const isDivisibleBy5 = (number % 5) === 0;
+    
+    if (isDivisibleBy5 && isDivisibleBy3) {
+        return 'FizzBuzz';
+    }
 
+    if (isDivisibleBy5) {
+        return 'Buzz';
+
+    }
+    if (isDivisibleBy3) {
+        return 'Fizz';
 }
-
+return number;
+};
 module.exports = fizzBuzz;
