3f4fb4104569d2c1c71feaaf5f5583dc5f42f4c2
Sam Jones <samueldjones24@gmail.com>
2019-02-16 15:44:55 +0000

    Completed humanCatDogYears test

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require('../src');
 
-// Look Ma, no handlebars!!!
+describe('humanCatDogYears', () => {
+    it('returns array of human, cat and dog years when passed human years', () => {
+        expect(humanCatDogYears(4)).toEqual([4, 32, 34])
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64])
+        expect(humanCatDogYears(17)).toEqual([17, 84, 99])
+    });
+});
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,25 @@
 const humanCatDogYears = (number) => {
-
+    const result = [number];
+    
+    if (number <= 1) {
+        result.push(15);
+    }
+    if (number <= 2) {
+        result.push(24); 
+    }
+    if (number > 2) {
+        result.push((number - 2) * 4 + 24);
+    }
+    if (number <= 1) {
+        result.push(15);
+    }
+    if (number <= 2) {
+        result.push(24); 
+    }
+    if (number > 2) {
+        result.push((number - 2) * 5 + 24);
+    }
+    return result;
 }
 
 module.exports = humanCatDogYears;
