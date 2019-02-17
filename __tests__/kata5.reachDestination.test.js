1b3bbbac06cc3e3dbfccdc28706cd4afcbd5a0e0
Sam Jones <samueldjones24@gmail.com>
2019-02-17 09:16:45 +0000

    Completed reachDestination test

--- __tests__/kata5.reachDestination.test.js
+++ __tests__/kata5.reachDestination.test.js
@@ -2,6 +2,8 @@ const { reachDestination } = require('../src');
 
 describe('reachDestination', () => {
   it('returns string with estimated time of arrival', () => {
-    
+    expect(reachDestination(44, 10)).toBe('I should be there in 4.5 hours')
+    expect(reachDestination(120, 70)).toBe('I should be there in 2 hours')
+    expect(reachDestination(310, 60)).toBe('I should be there in 5.5 hours')
   });
 });
--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,5 +1,10 @@
 const reachDestination = (distance, speed) => {
-
+    const roundHalf = (n) => {
+        return Math.ceil(n*2)/2;
+        }
+    const time = distance / speed;
+    const ETA = roundHalf(time);
+    return 'I should be there in ' + ETA + ' hours';
 }
 
 module.exports = reachDestination;
