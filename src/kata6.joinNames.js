d617a2c94eb0d8589837a7aa3d5a096c453fa2ea
Sam Jones <samueldjones24@gmail.com>
2019-02-19 18:23:31 +0000

    Completed joinNames & getEmployerRole tests

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,5 +1,21 @@
 const joinNames = (namesObj) => {
+    const getNames = namesObj.map(person => person.name);
+    let namesInAString = '';
 
+    if (getNames.length === 1) {
+        namesInAString = getNames[0];
+    }
+
+    if (getNames.length === 2) {
+        namesInAString = getNames.join(' & ');
+    }
+
+    if (getNames.length > 2) {
+        namesInAString = getNames.slice(0, -1).join(', ') + ' & ' + getNames.slice(-1);
+    }
+
+    return namesInAString;
 }
+
 
 module.exports = joinNames;
